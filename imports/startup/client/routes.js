0e483918da92948a350669d38be944d1d1778f8a
TJacoob <tomas.j.martins@ist.utl.pt>
2018-05-14 23:43:00 +0100

    Bootstrap Added
    - Also fontawesome(?)

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,13 +4,22 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/dashboard/dashboard.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
+/*
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
+  },
+});*/
+
+FlowRouter.route('/', {
+  name: 'Dashboard',
+  action() {
+    BlazeLayout.render('Dashboard');
   },
 });
 
